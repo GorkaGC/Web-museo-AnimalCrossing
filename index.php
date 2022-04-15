55c1eb538ead3d0d5d16cf2ec65af1216be3bde4
GorkaGC <gorka.garcia.castro@gmail.com>
2022-04-15 17:55:01 +0200

    15/04/2022 Footer hecho ( posibles cambios)

--- index.php
+++ index.php
@@ -1,15 +1,47 @@
 <!DOCTYPE html>
 <html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="styles.css">
+    <title>INDEX</title>
+</head>
+
 <body>
 
-<h1>My First Heading</h1>
-<p>My first paragraph.</p>
+    <h1>My First Heading</h1>
+    <p>TEST.</p>
+    <audio autoplay="" loop="" src="media/index.mp3"></audio>
 
 
 
 
-<footer>
-    hola soy el footer
-</footer>
+    <footer class="full-container">
+        <div class="contenido container">
+            <div class="contacto"> <a href="#"> Contacta con nosotros  </a></div>
+            
+            <div class="social">
+            <a href="#"> <i class="fa-brands fa-facebook" style="color:#3b5998"></i></a>
+            <a href="#"> <i class="fa-brands fa-twitter" style="color:#00acee"></i></a>
+            <a href="#"> <i class="fa-brands fa-youtube" style="color:#FF0000"></i></a>
+            <a href="#"> <i class="fa-brands fa-instagram" style="color:#8a3ab9"></i></a>
+            </div>
+
+            <div class="links">
+                <ul>
+                    <li>All rights reserved C</li>
+                    <li>Created by: <span>Nerea Martinez</span> and <span>Gorka García</span></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
 </body>
+
 </html>
