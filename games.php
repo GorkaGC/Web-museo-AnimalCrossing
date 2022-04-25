8a106100c5ae389161bad03109f43698d2f53e63
Nerea Martinez <neeotp1510@gmail.com>
2022-04-25 16:04:16 +0200

    pagina games.php

---
+++ games.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="styles.css">
+    <title>Juegos</title>
+</head>
+
+<body>
+
+    <header class="full-container">
+        <div class="navbar container">
+            <div class="icon-main">
+                <img src="media/icon_main1.png" alt="">
+            </div>
+            <div class="nav-links">
+                <a href="index.php" title="Historia">
+                    <img src="media/icon_history.png" alt="">
+                </a>
+                <a href="store.php" title="Tienda">
+                    <img src="media/icon_shop.png" alt="">
+                </a>
+                <a href="games.php" title="Juegos">
+                    <img src="media/icon_games.png" alt="">
+                </a>
+            </div>
+
+        </div>
+    </header>
+
+
+    <footer class="full-container">
+        <div class="contenido container">
+            <div class="contacto"> <a href="#"> Contacta con nosotros </a></div>
+
+            <div class="social">
+                <a href="#"> <i class="fa-brands fa-facebook" style="color:#3b5998"></i></a>
+                <a href="#"> <i class="fa-brands fa-twitter" style="color:#00acee"></i></a>
+                <a href="#"> <i class="fa-brands fa-youtube" style="color:#FF0000"></i></a>
+                <a href="#"> <i class="fa-brands fa-instagram" style="color:#8a3ab9"></i></a>
+            </div>
+
+            <div class="links">
+                <ul>
+                    <li>All rights reserved C</li>
+                    <li>Created by: <span>Nerea Martinez</span> and <span>Gorka García</span></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</body>
+
+
+
+</html>
