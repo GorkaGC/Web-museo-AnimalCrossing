d9565192938e86cb10affdb9a0040e978ff53b8e
Nerea Martinez <neeotp1510@gmail.com>
2022-05-04 20:59:39 +0200

    Creación archivos nav y footer

---
+++ nav.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+?>
+<header class="full-container">
+        <div class="navbar container">
+            <div class="icon-main">
+                <img src="media/icon_main1.png" alt="">
+            </div>
+            <div class="nav-links">
+                <a href="control.php?f=index" title="Historia">
+                    <img src="media/icon_history.png" alt="">
+                </a>
+                <a href="control.php?f=store" title="Tienda">
+                    <img src="media/icon_shop.png" alt="">
+                </a>
+                <a href="control.php?f=games" title="Juegos">
+                    <img src="media/icon_games.png" alt="">
+                </a>
+            </div>
+
+        </div>
+    </header>
