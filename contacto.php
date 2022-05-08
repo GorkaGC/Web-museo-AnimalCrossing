df358e054d48a11b3673a41a565d20f873bc492f
Gorka <gorka.garcia.castro@gmail.com>
2022-05-08 14:00:56 +0200

    PgContacto empezada 08/05/2022

---
+++ contacto.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Arvo:wght@400;700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="styles.css">
+    <title>CONTACTO</title>
+</head>
+
+<body>
+    <?php include "nav.php"; ?>
+
+    <div class="contacto-full full-container">
+        <div class="contacto full-container">
+            <div class="who">
+                <h1>¿QUIENES SOMOS?</h1>
+                <div class="who-content">
+                    <img src="media/icon_main1.png">
+                    <div class="who-text">
+                        <h3>Gorka</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum nesciunt blanditiis debitis deserunt unde repudiandae veniam odio nobis itaque accusantium porro amet sint provident eius modi aliquid, quibusdam aliquam ea.</p>
+                    </div>
+                </div>
+
+                <div class="who-content">
+                    <img src="media/icon_main1.png">
+                    <div class="who-text">
+                        <h3>Gorka</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum nesciunt blanditiis debitis deserunt unde repudiandae veniam odio nobis itaque accusantium porro amet sint provident eius modi aliquid, quibusdam aliquam ea.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="form-contact container">
+                <h1>CONTACTO</h1>
+            </div>
+        </div>
+    </div>
+    <?php include 'footer.php'; ?>
+</body>
+
+</html>
